48dc41d648ce005a21c95ba2d31b10699517b7bf
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-08-24 01:00:59 +0000

    fix linting

--- pages/swagger.go
+++ pages/swagger.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gofiber/swagger"
 )
 
+// AddSwagger Registers the swagger ui page with the fiber app
+// - app: *fiber.App fiber app
 func AddSwagger(app *fiber.App) {
 	app.Get("/swagger/*", swagger.HandlerDefault)
 }
